3ad712672efd7b8df1fd30377536ff0538908c2e
Ibrahim Tahayna <ibrahimt@aui.io>
2023-11-07 18:31:45 +0200

    first commit

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    preset: "jest-expo",
+    setupFilesAfterEnv: ["@testing-library/jest-native/extend-expect"],
+    transform: {
+      "^.+\\.(js|ts|tsx)$": "babel-jest",
+    },
+    moduleNameMapper: {
+      "\\.(png|jpg|jpeg|gif|svg)$": "<rootDir>/__mocks__/fileMock.js",
+    },
+  };
